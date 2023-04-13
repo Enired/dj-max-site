ac20d5e5a2a03416bfe9b10249d373aa12c31b94
Derine Banis <d.banis.13@gmail.com>
2023-04-12 23:55:43 -0700

    Rename

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import SongTile from './components/songTile';
+import SongTile from './components/SongTile';
 
 
 const listOfSongObjTEST = () => {
--- src/components/SongTile.js
+++ src/components/SongTile.js
@@ -1,3 +1,5 @@
+
+
 const SongTile = (props) => {
   return (
     <div>
