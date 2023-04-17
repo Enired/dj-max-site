1fe6ebb687fbaaac907b6c875125a9ecc72bc640
Derine Banis <d.banis.13@gmail.com>
2023-04-16 21:13:53 -0700

    Add button to remove item from carousel.

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 
-function Carousel({ items }) {
+function Carousel(props) {
   const [currentItem, setCurrentItem] = useState(0);
+  const [items, setItems] = useState(props.items)
 
   function handleNext() {
     setCurrentItem(currentItem === items.length - 1 ? 0 : currentItem + 1);
@@ -11,13 +12,21 @@ function Carousel({ items }) {
     setCurrentItem(currentItem === 0 ? items.length - 1 : currentItem - 1);
   }
 
+  function handleRemove(index) {
+    const newItems = [...items];
+    newItems.splice(index, 1);
+    setCurrentItem(currentItem === items.length - 1 ? 0 : currentItem);
+    setItems(newItems)
+  }
+
   return (
-    <div className="carousel" style={{'max-width' : '50%'}}>
+    <div className="carousel" style={{ 'max-width': '50%' }}>
       <div className="carousel-item">
         {items[currentItem]}
       </div>
-      <button style={{width: '50%'}} onClick={handlePrev}>Prev</button>
-      <button style={{width: '50%'}} onClick={handleNext}>Next</button>
+      <button onClick={() => handleRemove(currentItem)}>Remove</button>
+      <button style={{ width: '50%' }} onClick={handlePrev}>Prev</button>
+      <button style={{ width: '50%' }} onClick={handleNext}>Next</button>
     </div>
   );
 }
